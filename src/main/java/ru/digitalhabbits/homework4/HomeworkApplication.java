4dd8c486476a9dc6c992c274a5653eda9e1bc9ba
User <Vladislav.popckov.94@yandex.ru>
2024-09-29 10:33:06 +0300

    done

--- src/main/java/ru/digitalhabbits/homework4/HomeworkApplication.java
+++ src/main/java/ru/digitalhabbits/homework4/HomeworkApplication.java
@@ -6,10 +6,13 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 @SpringBootApplication
+@PropertySource("classpath:config/10-additional.properties")
+@PropertySource("classpath:config/20-additional.properties")
 public class HomeworkApplication
         implements ApplicationRunner {
     private static final Logger logger = getLogger(HomeworkApplication.class);
